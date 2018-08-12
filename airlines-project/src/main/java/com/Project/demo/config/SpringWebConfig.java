121983147e7511fc034fce250248f286b0bea9f3
SIMRAN RATHORE <simranrathore63@gmail.com>
2018-08-12 13:20:38 +0530

    changes

--- airlines-project/src/main/java/com/Project/demo/config/SpringWebConfig.java
+++ airlines-project/src/main/java/com/Project/demo/config/SpringWebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
@@ -17,9 +18,13 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter{
 	public ViewResolver getViewResolver(){
 		InternalResourceViewResolver internalResourceViewResolver=new InternalResourceViewResolver();
 		internalResourceViewResolver.setViewClass(JstlView.class);
-		internalResourceViewResolver.setPrefix("/WEB-INF/viewes/");
+		internalResourceViewResolver.setPrefix("/WEB-INF/views/");
 		internalResourceViewResolver.setSuffix(".jsp");
 		return internalResourceViewResolver;
 	}
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
 
 }
